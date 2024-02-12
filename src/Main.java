2d643b5c274390d6b5370093f63fb2495a2999b0
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-02-11 21:33:59 -0300

    exercicio 02

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,46 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int continuar;
+        int resposta;
         List<String> curso = new ArrayList<>();
         List<String> professor = new ArrayList<>();
 
+
         do {
-            System.out.println("Informar curso");
-            curso.add(scanner.nextLine());
-            System.out.println("Informar professor");
-            professor.add(scanner.nextLine());
-            System.out.println("Deseja continuar? 1 (sim) / 0 (nao)");
-            continuar = scanner.nextInt();
+            System.out.println("Menu de opção:");
+            System.out.println("(1) Listar curso / professor \n(2) Adicionar \n(0) Sair");
+            resposta = scanner.nextInt();
             scanner.nextLine();
+            if(resposta == 0){
+                break;
+            } else if (resposta == 1){
+                System.out.println("Lista de cursos:");
+                for(int i = 0; i < curso.size(); i++){
+                    System.out.println(i + " " + curso.get(i));
+                }
 
-        } while (continuar != 0);
+                System.out.println("--------------");
+                System.out.println("Lista de professores:");
+                for(int i = 0; i < professor.size(); i++){
+                    System.out.println(i + " " + professor.get(i));
+                }
+                System.out.println("--------------");
 
-        System.out.println("--------------");
-        System.out.println("Lista de cursos:");
-        for (String i : curso) {
-            System.out.println(i);
-        }
+            }else if(resposta == 2){
+                System.out.println("Informar curso");
+                curso.add(scanner.nextLine());
+                System.out.println("Informar professor");
+                professor.add(scanner.nextLine());
 
-        System.out.println("--------------");
-        System.out.println("Lista de professores:");
-        for (String i : professor) {
-            System.out.println(i);
-        }
+            }else {
+                System.out.println("Digite uma opção válida");
+            }
+        } while(true);
+
 
 
     }
+
+
 }
+
